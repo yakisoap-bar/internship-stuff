bff1b3fc878cd51bef483d81301c8c04b0b7fa94
Wooniety <wooniety@gmail.com>
2021-12-09 07:11:44 +0000

    Fixed nodemodule references
    Not the brightest mistake

--- live-analysis/frontend/app.js
+++ live-analysis/frontend/app.js
@@ -8,6 +8,7 @@ const app = express();
 // Making public folder to host
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
+app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/chart.js/dist'));
 
 // Start server on port
