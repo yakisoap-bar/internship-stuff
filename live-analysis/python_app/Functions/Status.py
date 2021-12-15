3ce5b9db56bc1b08efde6b75320597bbaa67d133
Ang Yak Hng <angyakhng8000@gmail.com>
2021-12-15 16:50:04 +0800

    update function calls for python app

--- live-analysis/python_app/Functions/Status.py
+++ live-analysis/python_app/Functions/Status.py
@@ -1,17 +1,36 @@
 from API.RSA_API import *
 from ctypes import *
-from Functions.Collect import err_check
+from Functions.Collect import err_check, rsa
+from time import sleep
 
-rsa = cdll.LoadLibrary('./API/win/RSA_API.dll')
+# rsa = cdll.LoadLibrary('./API/win/RSA_API.dll')
 
 def getBatteryStatus():
+    '''
+    Returns a dictionary containing the plugged-in Spectrum Analyser's battery status.
+
+    `plugged_in` : Whether the SA is plugged in
+    `batt_present` :Whether the SA contains a battery
+    `charge` : The battery charge level of the SA
+    `overtemp` : Whether the SA battery is above 45 deg C
+    `batt_hwerror` : Whether an error was detected in the battery hardware of the SA
+    '''
     # define struct
-    power_info = POWER_INFO()
+    pInfo = POWER_INFO()
 
     # get power info from SA
-    err_check(rsa.POWER_GetStatus(byref(power_info)))
+    sleep(1)
+    err_check(rsa.POWER_GetStatus(byref(pInfo)))
 
-    return power_info
+    output = {
+        'plugged_in' : pInfo.externalPowerPresent,
+        'batt_present' : pInfo.batteryPresent,
+        'charge' : pInfo.batteryChargeLevel,
+        'overtemp' : pInfo.batteryOverTemperature,
+        'batt_hwerror' : pInfo.batteryHardwareError
+    }
+
+    return output
 
 def rebootDevice(id):
     err_check(rsa.DEVICE_Reset(c_int(id)))
