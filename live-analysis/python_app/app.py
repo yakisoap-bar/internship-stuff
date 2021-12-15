6818bf7d485853f9ab3d9eb5ed2e758267457071
Wooniety <wooniety@gmail.com>
2021-12-15 17:25:38 +0800

    nice parsing works

--- live-analysis/python_app/app.py
+++ live-analysis/python_app/app.py
@@ -7,10 +7,8 @@ from Functions.Status import *
 
 # program
 def doStuff(args):
-	search_connect()
 	config_block_iq(args.centerFreq, args.refLevel, args.bandwidth, 1024)
-	data = acquire_block_iq(1024, args.numrecords).tolist()
-	print(getBatteryStatus())
+	data = acquire_block_iq(1024, args.numRecords).tolist()
 	print(predict_post('http://localhost:3000/predict', data))
 
 def main():
@@ -18,21 +16,33 @@ def main():
 	parser.add_argument('-v', '--verbose',
 						action='store_true', dest="verbose",
 						help='Display additional output and current configurations')
+	parser.add_argument('--battery',
+						action='store_true', dest="battery",
+						help='Check battery level')
 	parser.add_argument('-f', '--centerfreq',
-						nargs='?', default=2.44e9, metavar="centerFreq", type=int,
+						dest="centerFreq", metavar="centerFreq",
+						nargs='?', default=2.44e9, type=int,
 						help="Set center frequency")
 	parser.add_argument('-r', '--reflevel',
-						nargs='?', default=0, metavar="refLevel", type=int,
+						dest="refLevel", metavar="refLevel",
+						nargs='?', default=0, type=int,
 						help="Set reference level")
 	parser.add_argument('-b', '--bandwidth',
-						nargs='?', default=40e6, metavar="bandwidth", type=int,
+						dest="bandwidth", metavar="bandwidth",
+						nargs='?', default=40e6, type=int,
 						help="Set bandwidth")
 	parser.add_argument('-n', '--numrecords',
-						nargs='?', default=10, metavar="numRecords", type=int,
+						dest="numRecords", metavar="numRecords",
+						nargs='?', default=10, type=int,
 						help="Determine number of records")
 
 	args = parser.parse_args()
-	checkStuff(args)
+
+	device_connect()
+	if args.battery:
+		print(getBatteryStatus()[])
+	else:
+		doStuff(args)
 
 if __name__ == '__main__':
 	main()
