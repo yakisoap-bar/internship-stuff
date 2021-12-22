b6c1f009d00ffc01f1e75c94280d4ab43c4a5609
Wooniety <wooniety@gmail.com>
2021-12-22 15:49:55 +0800

    msPaint ftw

--- live-analysis/python_app/app.py
+++ live-analysis/python_app/app.py
@@ -1,12 +1,18 @@
 import sys, random
 from PySide6 import QtCore, QtWidgets, QtGui
 
-class Widget(QtWidgets.QWidget):
+class Window(QtWidgets.QWidget):
 	def __init__(self) -> None:
 		super().__init__()
 
+		self.setGeometry(50, 50, 500, 300)
+		self.setWindowTitle("Live Classification")
+		self.setWindowIcon(QtGui.QIcon('./img/icon.png'))
+
+		# Some variables
 		self.hello = ["End", "Me", "Plz"]
 
+		# Create some buttons
 		self.button = QtWidgets.QPushButton("PUSH BUTTON")
 		self.text = QtWidgets.QLabel("End me",
 									alignment=QtCore.Qt.AlignCenter)
@@ -15,18 +21,20 @@ class Widget(QtWidgets.QWidget):
 		self.layout.addWidget(self.text)
 		self.layout.addWidget(self.button)
 
-		self.button.clicked.connect(self.magic)
+		# On button click, call function
+		self.button.clicked.connect(self.changeText)
 
 	
 	@QtCore.Slot()
-	def magic(self):
+	def changeText(self):
 		self.text.setText(random.choice(self.hello))
 
+# Run the application
 if __name__ == "__main__":
 	app = QtWidgets.QApplication([])
+	GUI = Window()
 
-	widget = Widget()
-	widget.resize(800,600)
-	widget.show()
+	GUI.resize(800,600)
+	GUI.show()
 
 	sys.exit(app.exec())
