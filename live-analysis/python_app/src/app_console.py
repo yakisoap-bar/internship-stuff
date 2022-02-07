90617861f68740ef8fcb7f5df17d114825deb89f
Wooniety <wooniety@gmail.com>
2022-02-07 17:35:16 +0800

    halfway through config parsing

--- live-analysis/python_app/src/app_console.py
+++ live-analysis/python_app/src/app_console.py
@@ -1,17 +1,56 @@
 # Libraries
-import argparse
+import argparse, configparser
 
 from Functions.Collect import *
 from Functions.Request import *
 from Functions.Status import *
 
+def parseArgs():
+	parser = argparse.ArgumentParser(description='Do the application')
+	parser.add_argument('-v', '--verbose',
+						action='store_true', dest="verbose",
+						help='Display additional output and current configurations')
+	parser.add_argument('--battery',
+						action='store_true', dest="battery",
+						help='Check battery level')
+	parser.add_argument('-f', '--centerfreq',
+						dest="cf", metavar="centerFreq",
+						nargs='?', default=2.44e9, type=int,
+						help="Set center frequency")
+	parser.add_argument('-r', '--reflevel',
+						dest="refLevel", metavar="refLevel",
+						nargs='?', default=0, type=int,
+						help="Set reference level")
+	parser.add_argument('-b', '--bandwidth',
+						dest="bw", metavar="bandwidth",
+						nargs='?', default=40e6, type=int,
+						help="Set bandwidth")
+	parser.add_argument('-n', '--records',
+						dest="numRecords", metavar="numRecords",
+						nargs='?', default=10, type=int,
+						help="Determine number of records")
+	parser.add_argument('-s', '--sampfreq',
+						dest="sampFreq", metavar="samplingFrequency",
+						nargs='?', default=40, type=int,
+						help="Set sampling frequency")
+	parser.add_argument('--conf',
+						dest="conFile", metavar="configFile",
+						nargs='?', type=str,
+						help="Specify configuration file.")
+
+	return parser.parse_args()
+
 def checkArgs(args):
 	if args.configFile:
 		pass
 		return True
-
 	else:
 		return False
+
+def readConf(filename):
+	with open(filename, 'r') as f:
+		conf = str(f)
+	conf.split()
 
 # program
 def doStuff():
@@ -26,43 +65,8 @@ def doStuff():
 
 		# print([predictions["signalNames"], predictions["predictions"]])
 
-def parseArguments():
-	parser = argparse.ArgumentParser(description='Do the application')
-	parser.add_argument('-v', '--verbose',
-						action='store_true', dest="verbose",
-						help='Display additional output and current configurations')
-	parser.add_argument('--battery',
-						action='store_true', dest="battery",
-						help='Check battery level')
-	parser.add_argument('-f', '--centerfreq',
-						dest="centerFreq", metavar="centerFreq",
-						nargs='?', default=2.44e9, type=int,
-						help="Set center frequency")
-	parser.add_argument('-r', '--reflevel',
-						dest="refLevel", metavar="refLevel",
-						nargs='?', default=0, type=int,
-						help="Set reference level")
-	parser.add_argument('-b', '--bandwidth',
-						dest="bandwidth", metavar="bandwidth",
-						nargs='?', default=40e6, type=int,
-						help="Set bandwidth")
-	parser.add_argument('-n', '--numrecords',
-						dest="numRecords", metavar="numRecords",
-						nargs='?', default=10, type=int,
-						help="Determine number of records")
-	parser.add_argument('-s', '--samplingfrequency',
-						dest="samplingFrequency", metavar="samplingFrequency",
-						nargs='?', default=40, type=int,
-						help="Set sampling frequency")
-	parser.add_argument('--file',
-						dest="configFile", metavar="configFile",
-						nargs='?', type=str,
-						help="Specify configuration file.")
-
-	return parser.parse_args()
-
 def main():
-	args = parseArguments()
+	args = parseArgs()
 	device_connect()
 	print(getBatteryStatus())
 	exit()
