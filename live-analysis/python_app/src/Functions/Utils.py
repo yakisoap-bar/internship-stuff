08e226d4a188afd8eff9806b5d34c683fd98441d
Wooniety <wooniety@gmail.com>
2022-03-08 17:07:19 +0800

    Prettier terminal

--- live-analysis/python_app/src/Functions/Utils.py
+++ live-analysis/python_app/src/Functions/Utils.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, math
 
 def printStatus(info):
 	# Clears screan after printing information
@@ -18,26 +18,69 @@ def printStatus(info):
 	
 	print(msg)
 
-def printPrediction(predictions):
-	os.system('cls' if os.name == 'nt' else 'clear')
-	# for name, pred in zip(predictions[1]['signalNames'], predictions[1]['predictions']):
-	# 	print(f'{name}: {pred}')
+def bannerPadding(string, cols, rows, centered = True):
+	'''
+	How to center a div
 
-	msg = '\t'.join(predictions[1]['signalNames']) + '\n'
-	msg += '\t'.join([str(round(pred, 3)) for pred in predictions[1]['predictions']])
+	Parameters:
+	------
+	string : list
+		Message to go into the box, split by newlines/if line is too long, concat at char limit
+	cols : int
+		Width of terminal
+	rows : int
+		No. of rows to fill
+	centered : boolean
+		If false, at the top of the box
+	
+	Returns:
+	------
+	str
+		centered div
+	'''
+	box = ""
 
-	print(msg)
+	padding = "*" + " " * int(cols-2) + "*\n"
+	num_padding = rows - len(string)
 
-def createBanner(section = "", msg = ""):
-	columns, rows = shutil.get_terminal_size(fallback=(80, 24))
-	header = "*"*columns
+	box += padding * (math.floor(num_padding/2))
 
-	section_spaces = " " * (int(columns/2-len(section)/2))
+	for line in string:
+		num_spaces = (cols-len(line))/2 - 1
+		spaces1 = " " * (math.floor(num_spaces))
+		spaces2 = " " * (math.ceil(num_spaces))
+		box += f"*{spaces1}{line}{spaces2}*"
 
-	if msg != "":
-		msg_spaces = " " * (int(columns/2 - len(msg)/2))
-		header_name = f"{section}\n\n{msg_spaces}{msg}"
+	box += padding * (math.floor(num_padding/2))
 
-	banner = f"{header}\n\n{header_spaces}{section_name}\n\n{header}"
+	return box
+
+def createBanner(section, msg = ""):
+	cols, rows = shutil.get_terminal_size(fallback=(80, 24))
+
+	bannerh_border = "*" * cols
+	paddingv = 5
+	max_line_len = cols - paddingv*2
+	header_rows = 5
+	msg_rows = rows - header_rows - 3
+
+	# Split msg into clean lines
+	msg = msg.split('\n')
+	for i in range(len(msg)):
+		line_len = len(msg[i])
+		if line_len > max_line_len:
+			trunc_line = msg[i][:line_len-max_line_len]
+			msg.insert(i+1, trunc_line)	
+
+	# Section Header
+	banner = f"{bannerh_border}"
+	banner += bannerPadding([section], cols, header_rows)
+	banner += f"{bannerh_border}"
+
+	# Section content	
+	banner += bannerPadding(msg, cols, msg_rows)
+	banner += f"{bannerh_border}"
 
 	return banner
+
+print(createBanner("Categories", "Message in a bottle"))
